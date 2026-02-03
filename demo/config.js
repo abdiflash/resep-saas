a03e5379e61f30910a0385c910e8a2cff4a99e5d
abdiflash <abdiflash@gmail.com>
2026-02-03 22:58:58 +0700

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     USER_NAME: "Demo Chef Edueasy",
-    // Link CSV terbaru Anda
+    // Link CSV yang sudah diperbaiki (angka 0 bukan huruf O)
     SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1880I2lXlhKrbztukJTRUsO7NAbQJL6if9sDBlpWzesGePdnq2fcHtZ_IDa_BfjIjkg17p1YH-wfx/pub?gid=0&single=true&output=csv",
     MIDTRANS_CLIENT_KEY: "Mid-client-MK-5yLAVCdP0Yd_o",
     PLATFORM_FEE_PERCENT: 15
